3a818fefcb8838f26a87a63ced1e5b3aefee9532
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2020-12-11 00:00:25 +0000

    while loop

---
+++ while_loop.cpp
@@ -0,0 +1,35 @@
+// Copyright (C) 2002 Donggeun Lim All rights reserved.
+//
+// Created by Donggeun Lim
+// Created on December 2020
+// This program uses a nested if statement
+
+#include <iostream>
+#include <string>
+
+main() {
+    std::string positiveString;
+    int loopCounter = 0;
+    int sum = 0;
+    int positiveNumber;
+    std::cout << "Enter the positive number : ";
+    std::cin >> positiveString;
+    std::cout << "" << std::endl;
+
+    try {
+        positiveNumber = std::stoi(positiveString);
+
+
+        if (positiveNumber > 0) {
+            while (loopCounter <= positiveNumber) {
+                sum = sum + loopCounter;
+                loopCounter = loopCounter + 1;
+            }
+            std::cout << sum;
+        } else {
+            std::cout << "This year is not a leap year";
+        }
+    } catch (std::invalid_argument) {
+        std::cout << "That was not an integer";
+    }
+}
